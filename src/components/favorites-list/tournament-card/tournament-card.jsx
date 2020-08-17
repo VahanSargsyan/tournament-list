c20931a6cf098815e359974fe76205cda9c84c66
Vahan Sargsyan <vahansargsyan1310@gmail.com>
2020-08-18 00:10:14 +0400

    [add] dialog befor tournament deleting

--- src/components/favorites-list/tournament-card/tournament-card.jsx
+++ src/components/favorites-list/tournament-card/tournament-card.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { IMAGE_API_ROOT } from '../../../constants/constants';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { IMAGE_API_ROOT } from "../../../constants/constants";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 500,
-    margin: 5
+    margin: 5,
   },
 });
 
-export default function TournamentCard({data, deleteFavorite}) {
+export default function TournamentCard({ data, deleteFavorite }) {
   const classes = useStyles();
 
   return (
@@ -39,7 +39,14 @@ export default function TournamentCard({data, deleteFavorite}) {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary" onClick={() => deleteFavorite(data.id)}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() =>
+            window.confirm("Do you realy want to delete this tournament") &&
+            deleteFavorite(data.id)
+          }
+        >
           Remove tornament
         </Button>
       </CardActions>
